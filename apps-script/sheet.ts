0d27c5d7f1b34274d94dcc2dc54883209c87384d
hiroto <tohhiro@gmail.com>
2024-08-10 21:23:02 +0900

    型エラーを修正

--- apps-script/sheet.ts
+++ apps-script/sheet.ts
@@ -1,38 +1,43 @@
 interface sheetType {
-    WS_NAME: string
-    SET_VAL_ARRAY: string[]
-    sheet: () => any
-    getlastRow: () => number
-    setMailBody: () => void
+  WS_NAME: string;
+  SET_VAL_ARRAY: string[][];
+  sheet: () => GoogleAppsScript.Spreadsheet.Sheet | null;
+  getLastRow: () => number;
+  setMailBody: () => void;
+}
+
+class Sheet implements sheetType {
+  WS_NAME: string;
+  SET_VAL_ARRAY: string[][];
+
+  constructor(ws_name, set_val_array) {
+    this.WS_NAME = ws_name;
+    this.SET_VAL_ARRAY = set_val_array;
   }
 
+  // 有効なワークシートを返す
+  sheet() {
+    return SpreadsheetApp.getActiveSpreadsheet().getSheetByName(this.WS_NAME);
+  }
 
-  class Sheet implements sheetType{
+  // 最終行を取得
+  getLastRow() {
+    const sheet = this.sheet();
+    if (!sheet) return 1;
+    const lastRow =
+      sheet
+        .getRange(1, 1)
+        .getNextDataCell(SpreadsheetApp.Direction.DOWN)
+        .getRow() ?? 1;
+    return lastRow + 1;
+  }
 
-      WS_NAME: string
-      SET_VAL_ARRAY: string[]
-
-      constructor(ws_name, set_val_array){
-        this.WS_NAME = ws_name
-        this.SET_VAL_ARRAY = set_val_array
-      }
-
-      // 有効なワークシートを返す
-      sheet() {
-        return SpreadsheetApp.getActiveSpreadsheet().getSheetByName(this.WS_NAME)
-      }
-
-
-      // 最終行を取得
-      getlastRow() {
-        const lastRow = this.sheet().getRange(1, 1).getNextDataCell(SpreadsheetApp.Direction.DOWN).getRow()
-        return lastRow + 1
-      }
-
-
-      // メールの内容を配列にパースしたものをSpreadSheetに反映
-      setMailBody() {
-        this.sheet().getRange(this.getlastRow(), 1, this.SET_VAL_ARRAY.length, 3).setValues(this.SET_VAL_ARRAY)
-      }
-
-    }
+  // メールの内容を配列にパースしたものをSpreadSheetに反映
+  setMailBody() {
+    const sheet = this.sheet();
+    if (!sheet) return;
+    sheet
+      .getRange(this.getLastRow(), 1, this.SET_VAL_ARRAY.length, 3)
+      .setValues(this.SET_VAL_ARRAY);
+  }
+}
